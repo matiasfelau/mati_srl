ca3e9cb47e8dbe1ed3fc45a23c0efde5674f6fc0
matiasfelau <matiasfelau@gmail.com>
2025-05-15 09:11:35 -0300

    Corrección de errores en dos gráficos

--- metrica_uso_recursos.py
+++ metrica_uso_recursos.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 #uso en un periodo de tiempo
-dias = list(range(1, 9))
+dias = list(range(10, 18))
 
 uso_recursos_antes = [34, 36, 35, 34, 31, 37, 34, 35]
 
@@ -10,7 +10,7 @@ uso_recursos_despues = [2, 1, 3, 2, 1, 3, 2, 1]
 
 plt.figure(figsize=(12, 6))
 plt.plot(dias, uso_recursos_antes, label='Antes (Uso haciendo backup en horas pico)', color='red', marker='o')
-plt.plot(dias, uso_recursos_despues, label='Después (Uso haciendo backup en horas de baja demanda)', color='green', marker='s')
+plt.plot(dias, uso_recursos_despues, label='Después (Uso en horas pico sin backup)', color='green', marker='s')
 plt.title('Porcentaje de uso de recursos del servicio de backup')
 plt.xlabel('Hora')
 plt.ylabel('Porcentaje de uso de recursos (%)')
@@ -26,10 +26,10 @@ uso_recursos_antes_2 = [100, 100, 98, 100, 97, 97, 100, 99]
 uso_recursos_despues_2 = [62, 61, 63, 62, 61, 63, 62, 61]
 
 plt.figure(figsize=(12, 6))
-plt.plot(dias, uso_recursos_antes_2, label='Antes (Uso del sistema en horas pico)', color='red', marker='o')
-plt.plot(dias, uso_recursos_despues_2, label='Después (Uso del sistema en horas de baja demanda)', color='green', marker='s')
+plt.plot(dias, uso_recursos_antes_2, label='Antes (Uso del sistema en horas pico con backup)', color='red', marker='o')
+plt.plot(dias, uso_recursos_despues_2, label='Después (Uso del sistema en horas pico sin backup)', color='green', marker='s')
 plt.title('Porcentaje de uso de recursos del sistema utilizando el servicio de backup')
-plt.xlabel('Horas')
+plt.xlabel('Hora')
 plt.ylabel('Porcentaje de uso de recursos (%)')
 plt.xticks(dias)
 plt.legend()
